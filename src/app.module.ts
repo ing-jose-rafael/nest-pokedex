21cbef89e2793924ed1bf2bc03e76981843158cb
josecarreno <ing.jcarreno@gmail.com>
2022-08-02 12:51:52 -0500

    create SEED module and make http requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'), // referencia DB  
-    PokemonModule, CommonModule
+    PokemonModule, CommonModule, SeedModule
   ],
 })
 export class AppModule {}
