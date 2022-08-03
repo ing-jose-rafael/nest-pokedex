32735065e4306814195181daa9471381076fbfe8
josecarreno <ing.jcarreno@gmail.com>
2022-08-03 10:11:31 -0500

    Setting environment variables

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,13 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true, })
-    );
+      forbidNonWhitelisted: true, 
+      transform: true,
+      transformOptions:{
+        enableImplicitConversion: true,
+      }
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
