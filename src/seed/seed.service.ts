50336e90ea3da229e88e9e7dc9ced6b302071c72
josecarreno <ing.jcarreno@gmail.com>
2023-02-22 11:30:42 -0500

    formatter pretter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -7,38 +7,34 @@ import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 interface InsertPok {
-  name: string,
-  no: number,
+  name: string;
+  no: number;
 }
 
 @Injectable()
 export class SeedService {
-  
   constructor(
-    
-    @InjectModel( Pokemon.name )
-    private readonly pokemonModule:Model<Pokemon>,
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModule: Model<Pokemon>,
 
     private readonly http: AxiosAdapter,
-
-  ){}
+  ) {}
 
   async executeSeed() {
     await this.pokemonModule.deleteMany({}); // es igual a delete * from pokemons;
 
-    const pokemonToInsert:InsertPok[] = [];
-    
-    const data  = await  this.http.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
-    data.results.forEach(({ name, url })=>{
+    const pokemonToInsert: InsertPok[] = [];
+
+    const data = await this.http.get<PokeResponse>(
+      `https://pokeapi.co/api/v2/pokemon?limit=650`,
+    );
+    data.results.forEach(({ name, url }) => {
       const segments = url.split(`/`);
-      
+
       const no = +segments.at(-2);
       pokemonToInsert.push({ name, no });
-      
     });
-    await this.pokemonModule.insertMany( pokemonToInsert );
+    await this.pokemonModule.insertMany(pokemonToInsert);
     return `Seed Executed`;
   }
-
-  
 }
