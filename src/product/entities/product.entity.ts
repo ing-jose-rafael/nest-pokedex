f5a90c218eb5a624515ff47e1b14c5b62a1579a3
josecarreno <ing.jcarreno@gmail.com>
2023-02-25 11:11:08 -0500

    creando el schema de product

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,23 @@
-export class Product {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ type: Number, index: true })
+  price: number;
+
+  @Prop({ type: Number })
+  stock: number;
+
+  @Prop()
+  image: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
+ProductSchema.index({ price: 1, stock: -1 }); // 👈  indexa por dos propiedades
