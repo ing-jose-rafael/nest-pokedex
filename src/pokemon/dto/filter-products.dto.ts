4feb968511a74c09cb1e80b2828416ea564603a3
josecarreno <ing.jcarreno@gmail.com>
2023-02-25 11:10:24 -0500

    agregando dto para filtar productos

---
+++ src/pokemon/dto/filter-products.dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional, IsPositive, Min, ValidateIf } from 'class-validator';
+
+export class FilterProductsDto {
+  @IsOptional()
+  @IsPositive()
+  limit: number;
+
+  @IsOptional()
+  @Min(0)
+  offset: number;
+
+  @IsOptional()
+  @Min(0)
+  minPrice: number;
+
+  @ValidateIf((params) => params.minPrice)
+  @IsPositive()
+  maxPrice: number;
+}
