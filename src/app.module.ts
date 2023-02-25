351d1351bd150fe10ab18a847fe2aac1bc1a7d10
josecarreno <ing.jcarreno@gmail.com>
2023-02-25 10:54:44 -0500

    creando el recurso nest g res product

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validations';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
       validationSchema: JoiValidationSchema,
+      // isGlobal: true, // lo pone global a todos los modulos sin necesidad de hacer importes, solo se hace la inyeccion
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
@@ -23,6 +25,7 @@ import { JoiValidationSchema } from './config/joi.validations';
     PokemonModule,
     CommonModule,
     SeedModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
