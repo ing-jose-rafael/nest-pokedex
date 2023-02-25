925dabcc28471a53e6d3b6d6de111ab95ed1bbab
josecarreno <ing.jcarreno@gmail.com>
2023-02-25 11:11:35 -0500

    agregando validaciones dto product

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,50 @@
-export class CreateProductDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  // @ApiProperty({ description: `product's name` })
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  // ApiProperty()
+  readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  @Min(1)
+  // ApiProperty()
+  readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  // ApiProperty()
+  readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
+  // ApiProperty()
+  readonly image: string;
+  /*
+  @IsNotEmpty()
+  @ValidateNested()
+  // ApiProperty()
+  readonly category: CreateCategoryDto; 
+  */
+
+  /*@IsNotEmpty()
+  @IsMongoId()
+  readonly brand: string;
+  */
+}
