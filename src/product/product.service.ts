c963ede13b7a4bab469536a9ab88cb269b367ae5
josecarreno <ing.jcarreno@gmail.com>
2023-02-25 12:07:18 -0500

    servicio para crear un producto y mostrar todos

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectModel(Product.name)
+    private readonly productModule: Model<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    createProductDto.name = createProductDto.name.toLocaleLowerCase();
+    try {
+      const product = await this.productModule.create(createProductDto);
+      return product;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   findAll() {
-    return `This action returns all product`;
+    return this.productModule.find().select(`-__v`);
   }
 
   findOne(id: number) {
